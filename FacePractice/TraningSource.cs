24b20b4aacf3be5d5d08b930c23d7eeae4741f9e
huskyncu <110438091+huskyncu@users.noreply.github.com>
2022-09-21 15:07:49 +0800

    initial

--- FacePractice/TraningSource.cs
+++ FacePractice/TraningSource.cs
@@ -12,7 +12,7 @@ namespace FacePractice
 
         public static readonly string[] jinping = new string[] {
             "https://th.bing.com/th/id/OIP.F9bAjDjRZ2eVhGybJRKlowHaE5?pid=ImgDet&rs=1",
-            "https://news.1242.com/wp-content/uploads/2017/10/jpp025321783a-1.jpg;0,0&resize=640:*",
+            "https://news.1242.com/wp-content/uploads/2017/10/jpp025321783a-1.jpg",
             "https://zjnews.zjol.com.cn/ztjj/2017sjdzt/sjdqmtbd/201710/W020171025690874672386.jpg",
             "https://www.iza.ne.jp/kiji/world/images/201120/wor20112023080022-m1.jpg",
             "https://www.1242.com/lf/asset/uploads/2017/08/Xi_Jinping_2016a.jpg"
