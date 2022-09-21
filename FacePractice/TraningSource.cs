fc7c000d5ab7b659b138bfb5fde440b137823569
huskyncu <110438091+huskyncu@users.noreply.github.com>
2022-09-21 15:03:50 +0800

    initial

--- FacePractice/TraningSource.cs
+++ FacePractice/TraningSource.cs
@@ -8,32 +8,7 @@ namespace FacePractice
 {
     class TraningSource
     {
-        public static readonly string[] iuFaces = new string[] {
-           /* "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/iu-%E6%9D%8E%E7%9F%A5%E6%81%A9-1592068502.jpg?crop=0.502xw:1.00xh;0.498xw,0&resize=640:*",
-            "https://images.chinatimes.com/newsphoto/2019-10-18/900/20191018005061.jpg",
-            "https://i.epochtimes.com/assets/uploads/2020/02/191203045325100707-450x298.jpg",
-            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/00iu-new-drama-1562588257.jpg?crop=0.502xw:1.00xh;0.498xw,0&resize=640:*",
-            "https://i.epochtimes.com/assets/uploads/2019/12/191223052816100707-450x300.jpg",
-            "https://images.chinatimes.com/newsphoto/2020-08-24/900/20200824002897.jpg",
-            "https://s.yimg.com/ny/api/res/1.2/qseblKYHC9WVz7yjkYUU9Q--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NTAwO2g9NjI2/https://media.zenfs.com/zh-tw/mydaily.kr/60cf1cd62164dbc202588575e6947799",
-            "https://image2.stheadline.com/f/1296p810/0x0/100/none/1adc4d7918562d7ef30c6bf2a6026e89/stheadline/inewsmedia/20200413/_2020041317445990487.jpg",
-            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/19121201-1576138536.jpg?crop=0.506xw:1.00xh;0.494xw,0&resize=640:*",
-            "https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/10/29/realtime/7001911.jpg&x=0&y=0&sw=0&sh=0&sl=W&fw=1050",
-            "https://attach.setn.com/newsimages/2019/05/18/1930315-XXL.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTaQjYtZ40cTYpqkMYXvjFyA7HVVs-Hk46jnw&usqp=CAU",
-            "https://i1.wp.com/popbee.com/image/2018/10/IU_02.jpg?quality=95&",
-            "https://i.pinimg.com/originals/36/49/2e/36492e3c2338b1aaa5bd652fdc9279ae.jpg",
-            "https://img.ltn.com.tw/Upload/ent/page/800/2019/11/10/phpa2dNfH.jpg",
-            "https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2020/03/03/99/7546262.jpg&x=0&y=0&sw=0&sh=0&sl=W&fw=1050",
-            "https://og.momoshop.com.tw/1596096593746/goodsimg/0007/056/840/7056840_R.jpg",
-            "https://i1.wp.com/popbee.com/image/2018/09/IU-korean-singer-10-anniversary-concert-teaser.jpg?quality=95&",
-            "https://cdn2.ettoday.net/images/3614/d3614928.jpg",*/
-            "https://attach.setn.com/newsimages/2019/08/30/2102292-XXL.jpg",
-            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/19110802-1573206177.jpg?crop=0.506xw:1.00xh;0,0&resize=640:*",
-            "https://a.ksd-i.com/a/2020-03-04/124787-820426.jpg",
-            "https://images.newtalk.tw/resize_action2/600/album/news/375/5e5f7f91abf1c.JPG",
-            "https://images.chinatimes.com/newsphoto/2020-05-19/900/20200519002591.jpg"
-        };
+        
 
         public static readonly string[] jinping = new string[] {
             "https://th.bing.com/th/id/OIP.F9bAjDjRZ2eVhGybJRKlowHaE5?pid=ImgDet&rs=1",
